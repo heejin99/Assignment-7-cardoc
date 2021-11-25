637bc268ce67da122a01822772e405c29f3ac74e
heejin99 <wkdgmlwls30@naver.com>
2021-11-25 18:46:22 +0900

    Test: User Unit Test
    - Create Test
    - Faile Test (Duplicated User)

--- src/domain/user/user.service.spec.ts
+++ src/domain/user/user.service.spec.ts
@@ -1,18 +1,71 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { AuthService } from "../auth/auth.service";
+import { Users } from "../entities/user.entity";
+import { DuplicatedUserException } from "./exception/DuplicatedUserException";
+import { UserRepository } from "./user.repository";
 import { UserService } from "./user.service";
+
+const token = "makeToken";
+const mockAuthService = {
+	makeToken: jest.fn((user) => {
+		return token;
+	})
+};
+const mockUserRepository = {
+	findUser: jest.fn(),
+	createUser: jest.fn()
+};
+const userInfo = {
+	id: "testid",
+	password: "testpw"
+};
+const newUser = new Users();
+newUser.id = "testid";
+newUser.password = "testpw";
 
 describe("UserService", () => {
 	let service: UserService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [UserService]
+			providers: [
+				UserService,
+				{
+					provide: UserRepository,
+					useValue: mockUserRepository
+				},
+				{
+					provide: AuthService,
+					useValue: mockAuthService
+				}
+			]
 		}).compile();
 
 		service = module.get<UserService>(UserService);
 	});
 
-	it("should be defined", () => {
-		expect(service).toBeDefined();
+	describe("Create TEST", () => {
+		it("create Success", async () => {
+			// given
+			mockUserRepository.findUser.mockReturnValue(null);
+			// when
+			const result = await service.create(userInfo);
+			// then
+			expect(mockUserRepository.findUser).toHaveBeenCalledWith(
+				userInfo.id
+			);
+			expect(result).toEqual(token);
+		});
+		it("create Failed (Duplicated User)", async () => {
+			// given
+			mockUserRepository.findUser.mockReturnValue(newUser);
+			try {
+				//when
+				await service.create(userInfo);
+			} catch (err) {
+				// then
+				expect(err).toBeInstanceOf(DuplicatedUserException);
+			}
+		});
 	});
 });
